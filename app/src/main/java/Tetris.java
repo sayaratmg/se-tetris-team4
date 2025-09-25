6b7a475bb6622a28ce427e4eeffff32bf7db5e19
NIsik419 <81894972+NIsik419@users.noreply.github.com>
2025-09-25 23:30:58 +0900

    feat: 기본 참조구조

---
+++ app/src/main/java/Tetris.java
@@ -0,0 +1,10 @@
+import component.Board;;
+
+public class Tetris {
+
+	public static void main(String[] args) {
+		Board main = new Board();
+		main.setSize(400, 500);
+		main.setVisible(true);
+	}
+}
